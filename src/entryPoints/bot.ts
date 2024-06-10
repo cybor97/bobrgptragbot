6b84f04659dbe688ecb5d755008ee58498493805
cyborg97 <muhametshin.1997@gmail.com>
2024-06-10 21:48:11 +0200

    fix: error handling

--- src/entryPoints/bot.ts
+++ src/entryPoints/bot.ts
@@ -4,63 +4,68 @@ import { Message } from "telegraf/typings/core/types/typegram";
 const mentions = ["bobr", "bóbr", "бобр"];
 
 async function handleMessage(ctx: Context): Promise<void> {
-  if (!ctx.message || !ctx.chat) {
-    return;
+  try {
+    if (!ctx.message || !ctx.chat) {
+      return;
+    }
+    const endpointUrl = process.env.RAG_ENDPOINT_URL ?? null;
+    if (endpointUrl === null) {
+      throw new Error("No RAG endpoint URL provided");
+    }
+
+    // @ts-ignore
+    const text: string = ctx.message.text;
+    if (!text) {
+      return;
+    }
+
+    // @ts-ignore
+    const replyTo: Message | null = ctx.message.reply_to_message ?? null;
+    if (
+      ctx.chat.id !== ctx.message.from.id &&
+      !mentions.some((mention) => text.toLowerCase().startsWith(mention)) &&
+      !text.includes(ctx.botInfo.username) &&
+      !(replyTo && replyTo.from?.id === ctx.botInfo.id)
+    ) {
+      return;
+    }
+
+    if (!text) {
+      return;
+    }
+
+    const messages = [{ role: "user", content: text }];
+    // @ts-ignore
+    const replyText = replyTo?.text ?? null;
+    if (replyText) {
+      messages.unshift({ role: "assistant", content: replyText });
+    }
+
+    await ctx.sendChatAction("typing");
+    const res = await fetch(endpointUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: process.env.RAG_AUTH_TOKEN!,
+      },
+      body: JSON.stringify({ messages, data: { userId: ctx.message.from.id } }),
+    });
+    const data = await res.text();
+
+    const respText = data
+      .split("\n")
+      // 0:"something"
+      .map((line) => line.substring(3, line.length - 1))
+      .join("")
+      // replace formatting from RAG
+      .replace(/\\n/g, "\n")
+      .replace(/\\"/g, '"');
+
+    await ctx.reply(respText, { parse_mode: "Markdown" });
+  } catch (err) {
+    await ctx.reply("Coś poszło nie tak!");
+    console.error(err);
   }
-  const endpointUrl = process.env.RAG_ENDPOINT_URL ?? null;
-  if (endpointUrl === null) {
-    throw new Error("No RAG endpoint URL provided");
-  }
-
-  // @ts-ignore
-  const text: string = ctx.message.text;
-  if (!text) {
-    return;
-  }
-
-  // @ts-ignore
-  const replyTo: Message | null = ctx.message.reply_to_message ?? null;
-  if (
-    ctx.chat.id !== ctx.message.from.id &&
-    !mentions.some((mention) => text.toLowerCase().startsWith(mention)) &&
-    !text.includes(ctx.botInfo.username) &&
-    !(replyTo && replyTo.from?.id === ctx.botInfo.id)
-  ) {
-    return;
-  }
-
-  if (!text) {
-    return;
-  }
-
-  const messages = [{ role: "user", content: text }];
-  // @ts-ignore
-  const replyText = replyTo?.text ?? null;
-  if (replyText) {
-    messages.unshift({ role: "assistant", content: replyText });
-  }
-
-  await ctx.sendChatAction("typing");
-  const res = await fetch(endpointUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: process.env.RAG_AUTH_TOKEN!,
-    },
-    body: JSON.stringify({ messages, data: { userId: ctx.message.from.id } }),
-  });
-  const data = await res.text();
-
-  const respText = data
-    .split("\n")
-    // 0:"something"
-    .map((line) => line.substring(3, line.length - 1))
-    .join("")
-    // replace formatting from RAG
-    .replace(/\\n/g, "\n")
-    .replace(/\\"/g, '"');
-
-  await ctx.reply(respText);
 }
 
 export function initBot(): void {
